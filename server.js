56b1f0e91020c8f99f52f7247622290a84b2867f
sivakotireddy45 <sasibrucelee1@gmail.com>
2025-11-05 09:56:31 +0530

    Fix port for Render

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ import generateRoutes from './routes/generateRoutes.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
+
+
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
